a9e30845bb83581700c9354b9bd0c0225f85e2af
Amanda Lima <apandalima@gmail.com>
2022-11-28 10:13:31 -0300

    criacao do bd livraria

---
+++ database/migrations/2022_11_25_144320_create_editora_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEditoraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('editora', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('editora');
+    }
+}
