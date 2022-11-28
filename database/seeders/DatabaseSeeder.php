99817f8af4a36591f7a7d538c52ce7e6a4ce0c4a
Amanda Lima <apandalima@gmail.com>
2022-11-28 10:54:28 -0300

    Configuração do Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       $this->call(array(EditoraSeeder, GeneroSeeder, LivroHasGeneroSeeder, GeneroSeeder, LivroSeeder, AutorSeeder));  // \App\Models\User::factory(10)->create();
     }
 }
