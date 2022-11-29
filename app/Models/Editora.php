4d91933697bddff2f732bb400c0fc9210e9d82c7
Amanda Lima <apandalima@gmail.com>
2022-11-29 10:02:43 -0300

    criação ds models (tabelas)

---
+++ app/Models/Editora.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Editora extends Model
+{
+    protected $table = 'editora';
+    protected $hidden = [];
+    protected $appends = [];
+
+}
